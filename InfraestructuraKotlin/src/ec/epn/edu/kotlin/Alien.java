6bb74e1818b42560f47d35422743135535f701fc
WilsonCastro1988 <wilson.castro@epn.edu.ec>
2018-01-23 10:49:04 -0500

    GO first commit
    se añade proyecto kotlin para pruebas con GO-CD

---
+++ InfraestructuraKotlin/src/ec/epn/edu/kotlin/Alien.java
@@ -0,0 +1,16 @@
+package ec.epn.edu.kotlin;
+
+public class Alien {
+
+	
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
---
+++ InfraestructuraKotlin/target/classes/ec/epn/edu/kotlin/Main.kt
@@ -0,0 +1,13 @@
+import ec.epn.edu.kotlin.Alien
+
+
+fun main(args:Array<String>)
+{
+	
+	val alien = Alien()
+	
+	alien.nombre = "Kotlin alien"
+	println("Hello Kotlin Alien desde clase java: "+alien.nombre)
+}
+
+
